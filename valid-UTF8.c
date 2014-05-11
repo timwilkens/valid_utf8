f918d790e02a8948066f35b5b595c2cc7866e7e6
Tim Wilkens <timwilkensf1@gmail.com>
2014-05-11 08:24:06 -0700

    Switch to hex literals.

--- valid-UTF8.c
+++ valid-UTF8.c
@@ -70,18 +70,18 @@ is_valid_utf8(char *c, int size) {
 bool
 invalid_trailing_byte(char c) {
 // After the leading byte, we have n-1 occurrences of 10xxxxxx bytes
-  return (((c >> 6) & 11) != 10) ? true : false;
+  return (((c >> 6) & 0x3) != 0x2) ? true : false;
 }
 
 int
 number_of_bytes(char c) {
   // High bit set to 0 only on single byte sequences (same as ASCII).
-  if (((c >> 7) & 1) == 0) { return 1; }
+  if (((c >> 7) & 0x1) == 0) { return 1; }
   // All others have the first n bits set to 1 and the next 0.
   // e.g. 2 byte sequences have the first byte start with 110.
-  else if (((c >> 5) & 111) == 110) { return 2; }
-  else if (((c >> 4) & 1111) == 1110) { return 3; }
-  else if (((c >> 3) & 11111) == 11110) { return 4; }
+  else if (((c >> 5) & 0x7) == 0x6) { return 2; }
+  else if (((c >> 4) & 0xF) == 0xE) { return 3; }
+  else if (((c >> 3) & 0x1F) == 0x1E) { return 4; }
   // Not a valid UTF-8 sequence.
   else { return 100; }
 }
